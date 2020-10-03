d22d0e34142247b1a67163c208c860791319219c
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-03 15:30:09 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 6, "M", "brown", "Calm and Collected", 1)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "M", "black", "grizzly", 0)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", "2", "F", "white", "cunning", 1)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 6, "M", "navy", "structured", 1)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "F", "purple", "peaceful", 0)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 8, "M", "gray", "soothing", 0)
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 12, "F", "silver", "diabolical", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 6, "M", "brown", "Calm and Collected", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "M", "black", "grizzly", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", "2", "F", "white", "cunning", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 6, "M", "navy", "structured", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4, "F", "purple", "peaceful", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 8, "M", "gray", "soothing", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 12, "F", "silver", "diabolical", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, "1", "M", "white", "curious", 1)
